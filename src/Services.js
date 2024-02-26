090bec9936d8b45b3fdcce2870545ea7e053e665
liljdot <jehuoki@gmail.com>
2024-02-26 18:49:39 +0100

    updated services page

--- src/Services.js
+++ src/Services.js
@@ -53,9 +53,9 @@ function Services() {
                                 </div>
                                 <div className="col-md-6">
                                     {/*pic here*/}
-                                    <img src="images/services-banner.jpeg" alt="" />
+                                    <img src="images/red-crane.jpeg" alt="" />
                                 </div>
-                                
+
                             </div>
                         </div>
                         <div className="col-md-6 ">
@@ -65,10 +65,104 @@ function Services() {
                                     Crane
                                 </h2>
                                 <p>
-                                Floating crane, also known as a crane vessel, is a type of crane that is mounted on a  barge or a ship for use in marine construction projects.like seas and oil rigs.
+                                    Floating crane, also known as a crane vessel, is a type of crane that is mounted on a  barge or a ship for use in marine construction projects.like seas and oil rigs.
                                 </p>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </section>
+            <section className="about_section layout_padding">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-5 col-md-6">
+                            <div className="detail-box">
+                                <h2>
+                                    Arial Cranes
+                                </h2>
+                                <p>
+                                    Aerial Crane. Also known as flying cranes, aerial cranes are special types used to lift loads in areas that are inaccessible by land, oil rigs
+                                    <br />
+                                    <br />
+                                    Aerial cranes resemble helicopters in appearance. Heavy loads are carried by this type  of crane, They are also referred to as sky cranes.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="col-lg-7 col-md-6">
+                            <div className="img-box row">
+                                <div className="col-lg-6 col-md-6">
+                                    <img src="images/arial-crane-1.jpeg" alt="" />
+                                </div>
+                                <div className="col-lg-6 col-md-6">
+                                    <img src="images/arial-crane-2.jpeg" alt="" />
+                                </div>  
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="professional_section about crane_section layout_padding">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8">
+                            <div className="img-box row">
+                                <div className="col-lg-6 col-md-6">
+                                    {/*pic here*/}
+                                    <img src="images/truck-crane-1.jpeg" alt="" />
+                                </div>
+                                <div className="col-lg-6 col-md-6">
+                                    {/*pic here*/}
+                                    <img src="images/truck-crane-2.jpeg" alt="" />
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className="col-md-4 ">
+                            <div className="detail-box">
+                                <h2>
+                                    Truck <br />
+                                    Cranes
+                                </h2>
+                                <p>
+                                Truck Cranes. As the name suggests, truck cranes or truck-mounted cranes can travel  efficiently on the road due to their unique build. They are hydraulically powered lifting  machine mounted on a truck or trailer. They are unique not just for construction sites also serve for logistics purposes.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="about_section layout_padding">
+                <div className="container">
+                    <div className="row">
+                        <div className="detail-box">
+                            <h2 className="text-center">
+                                Generators
+                            </h2>
+                            <h4>
+                            We rebuild and maintenance industrial and marine generators & engines of different brands:
+                            </h4>
+                        </div>
+                    </div>
+                    <div className="row generator-brand">
+                        <div className="col"><a href="">Caterpillar Generators</a></div>
+                        <div className="col"><a href="">Atlas Copco</a></div>
+                        <div className="col"><a href="">Duetz</a></div>
+                        <div className="col"><a href="">FG Wilson Generators</a></div>
+                        <div className="col"><a href="">Fireman</a></div>
+                        <div className="col"><a href="">Kohler</a></div>
+                        <div className="col"><a href="">Cummins Generator</a></div>
+                        <div className="col"><a href="">Perkins</a></div>
+                        <div className="col"><a href="">Lister Peter</a></div>
+                        <div className="col"><a href="">John Deeref</a></div>
+                        <div className="col"><a href="">Detroit</a></div>
+                    </div>
+                    <div className="row img-box">
+                        <img src="" alt="" className="col-md-6" />
+                        <img src="" alt="" className="col-md-6" />
+                        <img src="" alt="" className="col-md-6" />
+                        <img src="" alt="" className="col-md-6" />
+                        <img src="" alt="" className="col-md-6" />
+                        <img src="" alt="" className="col-md-6" />
                     </div>
                 </div>
             </section>
