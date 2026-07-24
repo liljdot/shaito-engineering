bb3488730013830dfb688a336ef2617aa24b82e5
liljdot <jehuoki@gmail.com>
2026-07-24 16:49:23 +0100

    comment out import of Site component in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Site from './Site';
+// import Site from './Site';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 
